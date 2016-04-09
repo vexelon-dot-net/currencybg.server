d21cc016e3e2a084b5500c5072087baef12d1e80
Petar Petrov <petar.petrov.georgiev@gmail.com>
2016-04-09 22:19:17 +0200

    Added periodic currencies fetch

--- src/main/java/net/vexelon/currencybg/srv/Heartbeat.java
+++ src/main/java/net/vexelon/currencybg/srv/Heartbeat.java
@@ -1,25 +1,41 @@
 package net.vexelon.currencybg.srv;
 
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 
-import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.vexelon.currencybg.srv.db.models.CurrencyData;
+import net.vexelon.currencybg.srv.db.models.CurrencyLocales;
+import net.vexelon.currencybg.srv.remote.BNBSource;
+import net.vexelon.currencybg.srv.remote.Source;
+import net.vexelon.currencybg.srv.remote.SourceException;
 
 /**
- * Fetches currencies from remote server.
+ * Fetches currencies from remote server and imports them into the database.
  *
  */
 public class Heartbeat implements Runnable {
 
+	private static final Logger log = LoggerFactory.getLogger(Heartbeat.class);
+
 	@Override
 	public void run() {
-		System.out.println("--- heartbeat ---");
-
-		List<String> result = Lists.newArrayList();
-		for (int i = 0; i < 5; i++) {
-			result.add(Integer.toString(new Random().nextInt(10)));
+		log.info("Updating rates from remote server ...");
+		Source source = new BNBSource();
+		try {
+			Map<CurrencyLocales, List<CurrencyData>> downloadRates = source.downloadRates();
+			log.debug("Importing downloaded rates in database ...");
+			// TODO insert in db
+			for (Map.Entry<CurrencyLocales, List<CurrencyData>> rates : downloadRates.entrySet()) {
+				log.trace("*** downloaded locale: {}", rates.getKey());
+				for (CurrencyData currency : rates.getValue()) {
+					log.trace("Currency: {} ({}) = {}", currency.getName(), currency.getCode(), currency.getRate());
+				}
+			}
+		} catch (SourceException e) {
+			log.error("Could not download currencies from remote!", e);
 		}
-
-		// TODO fetch currencies from remote server and save to db
 	}
 }
