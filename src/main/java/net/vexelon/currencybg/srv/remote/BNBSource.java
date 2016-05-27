dcbd1d0acd8a3d407f392b4acedb3ffd4591540e
Petar Petrov <petar.petrov.georgiev@gmail.com>
2016-05-27 22:37:18 +0200

    Fix BNB unit tests

--- src/main/java/net/vexelon/currencybg/srv/remote/BNBSource.java
+++ src/main/java/net/vexelon/currencybg/srv/remote/BNBSource.java
@@ -2,17 +2,13 @@ package net.vexelon.currencybg.srv.remote;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
@@ -23,7 +19,6 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
-import com.google.common.io.ByteStreams;
 
 import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.db.models.Sources;
@@ -59,30 +54,84 @@ public class BNBSource extends AbstractSource {
 		super(reporter);
 	}
 
-	public List<CurrencyData> getBNBRates(InputStream input) throws Exception {
-		List<CurrencyData> listCurrencyData = Lists.newArrayList();
+	/**
+	 * Transforms XML data into {@link CurrencyData} models.
+	 * 
+	 * @param input
+	 * @return
+	 * @throws IOException
+	 * @throws XmlPullParserException
+	 */
+	public List<CurrencyData> getBNBRates(InputStream input) throws IOException, XmlPullParserException {
+		List<CurrencyData> result = Lists.newArrayList();
 
-		return listCurrencyData;
-	}
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		XmlPullParser parser = factory.newPullParser();
+		parser.setInput(input, Charsets.UTF_8.name());
 
-	private List<CurrencyData> getBNBRates(String sourceUrl) throws SourceException {
-		InputStream input = null;
-		try {
-			URLConnection connection = new URL(sourceUrl).openConnection();
-			connection.setDoInput(true);
-			HttpURLConnection httpConn = (HttpURLConnection) connection;
-			if (httpConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-				// read error and throw it to caller
-				input = httpConn.getErrorStream();
-				throw new SourceException(new String(ByteStreams.toByteArray(input), Charsets.UTF_8.name()));
+		DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		boolean isHeaderParsed = false;
+		StringBuilder buffer = new StringBuilder();
+		CurrencyData currencyData = new CurrencyData();
+
+		int eventType = parser.getEventType();
+
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			String tagName = parser.getName();
+			switch (eventType) {
+			case XmlPullParser.START_TAG:
+				if (XML_TAG_ROW.equalsIgnoreCase(tagName)) {
+					if (isHeaderParsed) {
+						currencyData = new CurrencyData();
+					}
+				}
+				buffer.setLength(0);
+				break;
+
+			case XmlPullParser.TEXT:
+				if (isHeaderParsed) {
+					buffer.append(parser.getText());
+					break;
+				}
+
+			case XmlPullParser.END_TAG:
+				if (isHeaderParsed) {
+					if (tagName.equalsIgnoreCase(XML_TAG_CODE)) {
+						currencyData.setCode(buffer.toString());
+					} else if (tagName.equalsIgnoreCase(XML_TAG_RATIO)) {
+						currencyData.setRatio(Integer.parseInt(buffer.toString()));
+					} else if (tagName.equalsIgnoreCase(XML_TAG_REVERSERATE)) {
+						currencyData.setBuy(buffer.toString());
+					} else if (tagName.equalsIgnoreCase(XML_TAG_RATE)) {
+						currencyData.setSell(buffer.toString());
+					} else if (tagName.equalsIgnoreCase(XML_TAG_CURR_DATE)) {
+						try {
+							currencyData.setDate(dateFormat.parse(buffer.toString()));
+						} catch (ParseException e1) {
+							log.warn("Could not parse date from buffer!", e1);
+							getReporter().write(TAG_NAME, "Could not parse date from buffer - '{}'! ({})",
+									buffer.toString(), e1.getMessage());
+							// use default (today)
+							currencyData.setDate(new Date());
+						}
+					} else if (XML_TAG_ROW.equalsIgnoreCase(tagName)) {
+						currencyData.setSource(Sources.BNB.getID());
+						result.add(currencyData);
+					}
+				}
+				if (!isHeaderParsed && XML_TAG_ROW.equalsIgnoreCase(tagName)) {
+					isHeaderParsed = true;
+				}
+				break;
+
+			default:
+				break;
 			}
-			input = httpConn.getInputStream();
-			return getBNBRates(input);
-		} catch (Exception e) {
-			throw new SourceException("Failed loading currencies from BNB source!", e);
-		} finally {
-			IOUtils.closeQuietly(input);
+			eventType = parser.next();
 		}
+
+		return result;
 	}
 
 	@Override
@@ -106,72 +155,7 @@ public class BNBSource extends AbstractSource {
 
 					if (!isCanceled) {
 						try {
-							XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-							factory.setNamespaceAware(true);
-							XmlPullParser parser = factory.newPullParser();
-							parser.setInput(response.getEntity().getContent(), Charsets.UTF_8.name());
-
-							DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-							boolean isHeaderParsed = false;
-							StringBuilder buffer = new StringBuilder();
-							CurrencyData currencyData = new CurrencyData();
-
-							int eventType = parser.getEventType();
-
-							while (eventType != XmlPullParser.END_DOCUMENT) {
-								String tagName = parser.getName();
-								switch (eventType) {
-								case XmlPullParser.START_TAG:
-									if (XML_TAG_ROW.equalsIgnoreCase(tagName)) {
-										if (isHeaderParsed) {
-											currencyData = new CurrencyData();
-										}
-									}
-									buffer.setLength(0);
-									break;
-
-								case XmlPullParser.TEXT:
-									if (isHeaderParsed) {
-										buffer.append(parser.getText());
-										break;
-									}
-
-								case XmlPullParser.END_TAG:
-									if (isHeaderParsed) {
-										if (tagName.equalsIgnoreCase(XML_TAG_CODE)) {
-											currencyData.setCode(buffer.toString());
-										} else if (tagName.equalsIgnoreCase(XML_TAG_RATIO)) {
-											currencyData.setRatio(Integer.parseInt(buffer.toString()));
-										} else if (tagName.equalsIgnoreCase(XML_TAG_REVERSERATE)) {
-											currencyData.setBuy(buffer.toString());
-										} else if (tagName.equalsIgnoreCase(XML_TAG_RATE)) {
-											currencyData.setSell(buffer.toString());
-										} else if (tagName.equalsIgnoreCase(XML_TAG_CURR_DATE)) {
-											try {
-												currencyData.setDate(dateFormat.parse(buffer.toString()));
-											} catch (ParseException e1) {
-												log.warn("Could not parse date from buffer!", e1);
-												getReporter().write(TAG_NAME,
-														"Could not parse date from buffer - '{}'! ({})",
-														buffer.toString(), e1.getMessage());
-												// use default (today)
-												currencyData.setDate(new Date());
-											}
-										} else if (XML_TAG_ROW.equalsIgnoreCase(tagName)) {
-											currencyData.setSource(Sources.BNB.getID());
-											result.add(currencyData);
-										}
-									}
-									if (!isHeaderParsed && XML_TAG_ROW.equalsIgnoreCase(tagName)) {
-										isHeaderParsed = true;
-									}
-									break;
-
-								default:
-									break;
-								}
-								eventType = parser.next();
-							}
+							result = getBNBRates(response.getEntity().getContent());
 						} catch (IOException e) {
 							log.error("Could not parse source data!", e);
 							getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
--- src/test/java/net/vexelon/currencybg/srv/remote/BNBSourceTests.java
+++ src/test/java/net/vexelon/currencybg/srv/remote/BNBSourceTests.java
@@ -37,29 +37,30 @@ public class BNBSourceTests {
 		try {
 			List<CurrencyData> rates = new BNBSource(new NullReporter())
 					.getBNBRates(getTestResource("/exchange_rates_en.xml"));
+
 			assertTrue(rates.get(0).getCode().equals("AUD"));
 			assertTrue(rates.get(0).getRatio() == 1);
-			// assertTrue(rates.get(0).getReverseRate().equals("0.760647"));
-			// assertTrue(rates.get(0).getRate().equals("1.31467"));
+			assertTrue(rates.get(0).getBuy().equals("0.760647"));
+			assertTrue(rates.get(0).getSell().equals("1.31467"));
 			assertTrue(rates.get(0).getDate().equals(newDate(2016, 3, 15))); // 15.03.2016
 
 			assertTrue(rates.get(3).getCode().equals("CHF"));
 			assertTrue(rates.get(3).getRatio() == 1);
-			// assertTrue(rates.get(3).getReverseRate().equals("0.56007"));
-			// assertTrue(rates.get(3).getRate().equals("1.78549"));
+			assertTrue(rates.get(3).getBuy().equals("0.56007"));
+			assertTrue(rates.get(3).getSell().equals("1.78549"));
 			assertTrue(rates.get(3).getDate().equals(newDate(2016, 3, 15))); // 15.03.2016
 
 			assertTrue(rates.get(14).getCode().equals("ISK"));
 			assertTrue(rates.get(14).getRatio() == 0);
-			// assertTrue(rates.get(14).getReverseRate().equals("0"));
-			// assertTrue(rates.get(14).getRate().equals("0"));
+			assertTrue(rates.get(14).getSell().equals("0"));
+			assertTrue(rates.get(14).getBuy().equals("0"));
 			assertTrue(rates.get(14).getDate().equals(newDate(2016, 3, 15))); // 15.03.2016
 
 			int pos = rates.size() - 1;
 			assertTrue(rates.get(pos).getCode().equals("XAU"));
 			assertTrue(rates.get(pos).getRatio() == 1);
-			// assertTrue(rates.get(pos).getReverseRate().equals("0"));
-			// assertTrue(rates.get(pos).getRate().equals("2175.97"));
+			assertTrue(rates.get(pos).getBuy().equals("0"));
+			assertTrue(rates.get(pos).getSell().equals("2175.97"));
 			assertTrue(rates.get(pos).getDate().equals(newDate(2016, 3, 15))); // 15.03.2016
 
 		} catch (Exception e) {
@@ -67,50 +68,5 @@ public class BNBSourceTests {
 			fail(e.getMessage());
 		}
 	}
-
-	// TODO - Вече теглим само единия файл. да се решо да има нужда от 2
-	// теставввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввввсс
-	/*
-	 * @Test public void testBG() { try { List<CurrencyData> rates = new
-	 * BNBSource().getBNBRates(getTestResource("/exchange_rates_bg.xml"));
-	 * assertTrue(rates.get(0).getName().equals("Австралийски долар"));
-	 * assertTrue(rates.get(0).getGold() == 1);
-	 * assertTrue(rates.get(0).getCode().equals("AUD"));
-	 * assertTrue(rates.get(0).getRatio() == 1);
-	 * System.out.println(rates.get(0).getReverseRate());
-	 * assertTrue(rates.get(0).getReverseRate().equals("0.754973"));
-	 * assertTrue(rates.get(0).getRate().equals("1.32455"));
-	 * assertTrue(rates.get(0).getCurrDate().equals(newDate(2016, 3, 11))); //
-	 * 11.03.2016 assertTrue(rates.get(0).getfStar() == 0);
-	 * 
-	 * assertTrue(rates.get(3).getName().equals("Швейцарски франк"));
-	 * assertTrue(rates.get(3).getGold() == 1);
-	 * assertTrue(rates.get(3).getCode().equals("CHF"));
-	 * assertTrue(rates.get(3).getRatio() == 1);
-	 * assertTrue(rates.get(3).getReverseRate().equals("0.559763"));
-	 * assertTrue(rates.get(3).getRate().equals("1.78647"));
-	 * assertTrue(rates.get(3).getCurrDate().equals(newDate(2016, 3, 11))); //
-	 * 11.03.2016 assertTrue(rates.get(3).getfStar() == 0);
-	 * 
-	 * assertTrue(rates.get(14).getName().equals("Исландска крона *"));
-	 * assertTrue(rates.get(14).getGold() == 1);
-	 * assertTrue(rates.get(14).getCode().equals("ISK"));
-	 * assertTrue(rates.get(14).getRatio() == 0);
-	 * assertTrue(rates.get(14).getReverseRate().equals("0"));
-	 * assertTrue(rates.get(14).getRate().equals("0"));
-	 * assertTrue(rates.get(14).getCurrDate().equals(newDate(2016, 3, 11))); //
-	 * 11.03.2016 assertTrue(rates.get(14).getfStar() == 1);
-	 * 
-	 * int pos = rates.size() - 1; assertTrue(rates.get(pos).getName().equals(
-	 * "Злато (1 трой унция)")); assertTrue(rates.get(pos).getGold() == 2);
-	 * assertTrue(rates.get(pos).getCode().equals("XAU"));
-	 * assertTrue(rates.get(pos).getRatio() == 1);
-	 * assertTrue(rates.get(pos).getReverseRate().equals("0"));
-	 * assertTrue(rates.get(pos).getRate().equals("2222.79"));
-	 * assertTrue(rates.get(pos).getCurrDate().equals(newDate(2016, 3, 11))); //
-	 * 11.03.2016 assertTrue(rates.get(pos).getfStar() == 0);
-	 * 
-	 * } catch (Exception e) { e.printStackTrace(); fail(e.getMessage()); } }
-	 */
 
 }
