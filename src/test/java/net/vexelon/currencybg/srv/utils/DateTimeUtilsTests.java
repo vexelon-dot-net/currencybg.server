9a5c2d1e70249a6fa06ac8fb93e0b1d7a32e10af
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2016-08-18 21:34:06 +0300

    Test5

--- src/test/java/net/vexelon/currencybg/srv/utils/DateTimeUtilsTests.java
+++ src/test/java/net/vexelon/currencybg/srv/utils/DateTimeUtilsTests.java
@@ -9,32 +9,32 @@ import net.vexelon.currencybg.srv.Defs;
 
 public class DateTimeUtilsTests {
 
-	// @Test
-	// public void test_Convert_TimeZone() {
-	// // fail("Not yet implemented");
-	// // assertTrue(1 == 1);
-	//
-	// try {
-	// assertTrue(DateTimeUtils
-	// .toTimeZone("2016-07-16T21:40:06+0200", Defs.CURRENT_TIME_ZONE,
-	// Defs.DATETIME_ZONE_FORMAT)
-	// .equals("2016-07-16T22:40:06+0300"));
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// fail(e.getMessage());
-	// }
-	// }
-
 	@Test
-	public void test_Modify_Date_Layout() {
+	public void test_Convert_TimeZone() {
+		// fail("Not yet implemented");
+		// assertTrue(1 == 1);
+
 		try {
 			assertTrue(DateTimeUtils
-					.modifyDateLayout("2016-07-16T22:40:06+0300", Defs.DATETIME_ZONE_FORMAT, Defs.DATETIME_FORMAT)
-					.equals("2016-07-16 22:40:06"));
+					.toTimeZone("2016-07-16T21:40:06+0200", Defs.CURRENT_TIME_ZONE, Defs.DATETIME_ZONE_FORMAT)
+					.equals("2016-07-16T22:40:06+0300"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
+
+	// @Test
+	// public void test_Modify_Date_Layout() {
+	// try {
+	// assertTrue(DateTimeUtils
+	// .modifyDateLayout("2016-07-16T22:40:06+0300", Defs.DATETIME_ZONE_FORMAT,
+	// Defs.DATETIME_FORMAT)
+	// .equals("2016-07-16 22:40:06"));
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// fail(e.getMessage());
+	// }
+	// }
 
 }
