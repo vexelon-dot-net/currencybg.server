448136cf5b8ddcfa79b49652f12b96eb84b3f3db
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2016-08-18 21:32:11 +0300

    Test4

--- src/test/java/net/vexelon/currencybg/srv/utils/DateTimeUtilsTests.java
+++ src/test/java/net/vexelon/currencybg/srv/utils/DateTimeUtilsTests.java
@@ -9,20 +9,21 @@ import net.vexelon.currencybg.srv.Defs;
 
 public class DateTimeUtilsTests {
 
-	@Test
-	public void test_Convert_TimeZone() {
-		// fail("Not yet implemented");
-		// assertTrue(1 == 1);
-
-		try {
-			assertTrue(DateTimeUtils
-					.toTimeZone("2016-07-16T21:40:06+0200", Defs.CURRENT_TIME_ZONE, Defs.DATETIME_ZONE_FORMAT)
-					.equals("2016-07-16T22:40:06+0300"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-	}
+	// @Test
+	// public void test_Convert_TimeZone() {
+	// // fail("Not yet implemented");
+	// // assertTrue(1 == 1);
+	//
+	// try {
+	// assertTrue(DateTimeUtils
+	// .toTimeZone("2016-07-16T21:40:06+0200", Defs.CURRENT_TIME_ZONE,
+	// Defs.DATETIME_ZONE_FORMAT)
+	// .equals("2016-07-16T22:40:06+0300"));
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// fail(e.getMessage());
+	// }
+	// }
 
 	@Test
 	public void test_Modify_Date_Layout() {
