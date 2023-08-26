f08d38625e6512e1e1a865e9e560436ca3811af8
Petar Petrov <ppetrov@vexelon.net>
2023-08-27 00:23:36 +0200

    Display listen server and port in logs

--- src/main/java/net/vexelon/currencybg/srv/apix/ApiVerticle.java
+++ src/main/java/net/vexelon/currencybg/srv/apix/ApiVerticle.java
@@ -5,8 +5,12 @@ import io.vertx.ext.web.Router;
 import net.vexelon.currencybg.srv.Defs;
 import net.vexelon.currencybg.srv.apix.junctions.Currencies;
 import net.vexelon.currencybg.srv.apix.junctions.Root;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApiVerticle extends AbstractVerticle {
+
+	private static final Logger log = LoggerFactory.getLogger(ApiVerticle.class);
 
 	private final Runnable onStopHook;
 
@@ -22,7 +26,9 @@ public class ApiVerticle extends AbstractVerticle {
 		new Currencies().attach(router);
 		new Root().attach(router);
 
-		vertx.createHttpServer().requestHandler(router).listen(Integer.parseInt(Defs.CONFIG_PORT));
+		log.info("Starting server on {}:{}", Defs.CONFIG_HOST, Defs.CONFIG_PORT);
+
+		vertx.createHttpServer().requestHandler(router).listen(Integer.parseInt(Defs.CONFIG_PORT), Defs.CONFIG_HOST);
 	}
 
 	@Override
