51129cb61d611f23a7cdd1a277d741b2f5911bb6
Petar Petrov <ppetrov@vexelon.net>
2023-09-02 21:42:55 +0200

    Fix Altcoints unit tests

--- src/main/java/net/vexelon/currencybg/srv/remote/AltcoinsSource.java
+++ src/main/java/net/vexelon/currencybg/srv/remote/AltcoinsSource.java
@@ -1,9 +1,6 @@
 package net.vexelon.currencybg.srv.remote;
 
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import net.vexelon.currencybg.srv.Defs;
@@ -11,7 +8,6 @@ import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.db.models.Sources;
 import net.vexelon.currencybg.srv.reports.Reporter;
 import net.vexelon.currencybg.srv.utils.DateTimeUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
@@ -19,123 +15,111 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Type;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class AltcoinsSource extends AbstractSource {
 
-    private static final Logger log = LoggerFactory.getLogger(AltcoinsSource.class);
-    private static final String TAG_NAME = AltcoinsSource.class.getSimpleName();
+	private static final Logger log      = LoggerFactory.getLogger(AltcoinsSource.class);
+	private static final String TAG_NAME = AltcoinsSource.class.getSimpleName();
 
-    private static final String URL_SOURCE = "https://altcoins.bg/currencies/rates";
-    private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
+	private static final String URL_SOURCE  = "https://altcoins.bg/currencies/rates";
+	private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
 
-    public static final Set<String> CURRENCIES =
-            Sets.newHashSet(Defs.CURRENCY_BITCOIN, Defs.CURRENCY_ETHERIUM, Defs.CURRENCY_LITECOIN,
-                    Defs.CURRENCY_RIPPLE, Defs.CURRENCY_BITCOIN_CASH, Defs.CURRENCY_DASH, Defs.CURRENCY_MONERO,
-                    Defs.CURRENCY_ZCASH, Defs.CURRENCY_STELLAR, Defs.CURRENCY_EOS);
+	public static final Set<String> CURRENCIES = Sets.newHashSet(Defs.CURRENCY_BITCOIN, Defs.CURRENCY_ETHERIUM,
+			Defs.CURRENCY_LITECOIN, Defs.CURRENCY_RIPPLE, Defs.CURRENCY_BITCOIN_CASH, Defs.CURRENCY_DASH,
+			Defs.CURRENCY_MONERO, Defs.CURRENCY_ZCASH, Defs.CURRENCY_STELLAR, Defs.CURRENCY_EOS);
 
-    public AltcoinsSource(Reporter reporter) {
-        super(reporter);
-    }
+	public AltcoinsSource(Reporter reporter) {
+		super(reporter);
+	}
 
-    /**
-     * Transforms Altcoins json data into {@link CurrencyData} models.
-     *
-     * @return Parsed currencies.
-     */
-    public List<CurrencyData> getAltcoinsRates(InputStream input) throws IOException, ParseException {
-        List<CurrencyData> result = Lists.newArrayList();
+	/**
+	 * Transforms Altcoins json data into {@link CurrencyData} models.
+	 *
+	 * @return Parsed currencies.
+	 */
+	public List<CurrencyData> getAltcoinsRates(InputStream input) throws IOException, ParseException {
+		var result = new ArrayList<CurrencyData>();
 
-        try {
-            String currentDateTimeSofia = LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA))
-                    .format(DateTimeFormatter.ofPattern(DATE_FORMAT)).toString();
+		try (var reader = new InputStreamReader((input))) {
+			Map<String, List<String>> pairs = new GsonBuilder().create()
+					.fromJson(reader, new TypeToken<Map<String, List<String>>>() {}.getType());
+			var updateDate = DateTimeUtils.parseDate(LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA))
+					.format(DateTimeFormatter.ofPattern(DATE_FORMAT)), DATE_FORMAT);
 
-            Date updateDate = DateTimeUtils.parseDate(currentDateTimeSofia, DATE_FORMAT);
+			for (Map.Entry<String, List<String>> next : pairs.entrySet()) {
+				if (CURRENCIES.contains(next.getKey()) && next.getValue().size() > 1) {
+					var currencyData = new CurrencyData();
+					currencyData.setCode(next.getKey());
+					currencyData.setBuy(next.getValue().get(0));
+					currencyData.setSell(next.getValue().get(1));
+					currencyData.setRatio(1);
+					currencyData.setSource(Sources.ALTCOINS.getID());
+					currencyData.setDate(updateDate);
+					result.add(currencyData);
+				} else if (log.isInfoEnabled()) {
+					log.info("Buy/sell not available or unexpected info present in '{}'! Size={}", next.getKey(),
+							next.getValue().size());
+				}
+			}
 
-            String inputString = IOUtils.toString(input, Charsets.UTF_8.name());
+			return normalizeCurrencyData(result);
+		} catch (RuntimeException e) {
+			throw new IOException(e);
+		}
+	}
 
-            Gson gson = new GsonBuilder().create();
-            Type type = new TypeToken<Map<String, List<String>>>() {
-            }.getType();
-            Map<String, List<String>> pairs = gson.fromJson(inputString, type);
+	@Override
+	public void getRates(Callback callback) throws SourceException {
+		try {
+			doGet(URL_SOURCE, new HTTPCallback() {
 
-            // {"BTC":[15273.172992,14557.243008],"ETH":[1347.9194792,1289.8877208],"LTC":[345.0662124,323.0193876],"XRP":[1.498323568,1.328702032],"DASH":[954.4866212,898.8854588],"XMR":[445.329248,411.073152],"ETC":[57.4290192,50.9276208],"ZEC":[634.818912,585.986688],"XLM":[0.5588930984,0.4956221816]}
-            // {"BTC":[15231.025152,14517.070848],"ETH":[1344.894768,1286.993232],"LTC":[343.68943,321.73057],"XRP":[1.49815736,1.32855464],"DASH":[952.6273064,897.1344536],"XMR":[445.18656,410.94144],"ETC":[57.2025608,50.7267992],"ZEC":[635.043136,586.193664],"XLM":[0.5588494688,0.4955834912]}
+				@Override
+				public void onRequestFailed(Exception e) {
+					getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
 
-            for (Map.Entry<String, List<String>> next : pairs.entrySet()) {
-                if (CURRENCIES.contains(next.getKey()) && next.getValue().size() > 1) {
-                    CurrencyData currencyData = new CurrencyData();
-                    currencyData.setCode(next.getKey());
-                    currencyData.setBuy(next.getValue().get(0));
-                    currencyData.setSell(next.getValue().get(1));
-                    currencyData.setRatio(1);
-                    currencyData.setSource(Sources.ALTCOINS.getID());
-                    currencyData.setDate(updateDate);
-                    result.add(currencyData);
-                } else if (log.isInfoEnabled()) {
-                    log.info("Buy/sell not available or unexpected info present in '{}'! Size={}", next.getKey(), next.getValue().size());
-                }
-            }
+					AltcoinsSource.this.close();
+					callback.onFailed(e);
+				}
 
-            return normalizeCurrencyData(result);
-        } catch (RuntimeException e) {
-            throw new IOException(e);
-        }
-    }
+				@Override
+				public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
+					var result = new ArrayList<CurrencyData>();
 
-    @Override
-    public void getRates(Callback callback) throws SourceException {
-        try {
-            doGet(URL_SOURCE, new HTTPCallback() {
+					if (!isCanceled) {
+						try {
+							result.addAll(getAltcoinsRates(response.getEntity().getContent()));
+						} catch (IOException | ParseException e) {
+							log.error("Could not parse source data!", e);
+							getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
+						}
+					} else {
+						log.warn("Request was canceled! No currencies were downloaded.");
+						getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
+					}
 
-                @Override
-                public void onRequestFailed(Exception e) {
-                    getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
+					AltcoinsSource.this.close();
+					callback.onCompleted(result);
+				}
+			});
+		} catch (URISyntaxException e) {
+			throw new SourceException("Invalid source url - " + URL_SOURCE, e);
+		}
 
-                    AltcoinsSource.this.close();
-                    callback.onFailed(e);
-                }
+	}
 
-                @Override
-                public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
-                    List<CurrencyData> result = Lists.newArrayList();
-
-                    if (!isCanceled) {
-                        try {
-
-                            result = getAltcoinsRates(response.getEntity().getContent());
-                        } catch (IOException | ParseException e) {
-                            log.error("Could not parse source data!", e);
-                            getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e)
-                            );
-                        }
-                    } else {
-                        log.warn("Request was canceled! No currencies were downloaded.");
-                        getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
-                    }
-
-                    AltcoinsSource.this.close();
-                    callback.onCompleted(result);
-                }
-            });
-        } catch (URISyntaxException e) {
-            throw new SourceException("Invalid source url - " + URL_SOURCE, e);
-        }
-
-    }
-
-    @Override
-    public String getName() {
-        return TAG_NAME;
-    }
+	@Override
+	public String getName() {
+		return TAG_NAME;
+	}
 
 }
--- src/test/java/net/vexelon/currencybg/srv/remote/AltcoinsSourceTest.java
+++ src/test/java/net/vexelon/currencybg/srv/remote/AltcoinsSourceTest.java
@@ -1,58 +1,56 @@
 package net.vexelon.currencybg.srv.remote;
 
-import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.reports.ConsoleReporter;
 import net.vexelon.currencybg.srv.tests.TestUtils;
 import org.junit.Test;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AltcoinsSourceTest {
 
-    @Test
-    public void test_AltcoinsSource01() {
+	@Test
+	public void test_AltcoinsSource01() {
 
-        try {
-            List<CurrencyData> rates = new AltcoinsSource(new ConsoleReporter())
-                    .getAltcoinsRates(TestUtils.getTestResource("/altcoins.json"));
+		try {
+			var rates = new AltcoinsSource(new ConsoleReporter()).getAltcoinsRates(
+					TestUtils.getTestResource("/altcoins.json"));
 
-            // XXX buy/sell rates - size of column is 10!
-            // This is not good, because it strips rate infos
-            // and the tests should be modified once the db and backend are updated!
+			// XXX buy/sell rates - size of column is 10!
+			// This is not good, because it strips rate infos
+			// and the tests should be modified once the db and backend are updated!
 
-            assertEquals("BTC", rates.get(0).getCode());
-            assertEquals(1, rates.get(0).getRatio());
-            assertEquals("18057.3255", rates.get(0).getBuy()); // 18057.325552
-            assertEquals("17226.7095", rates.get(0).getSell());
+			assertEquals("BTC", rates.get(0).getCode());
+			assertEquals(1, rates.get(0).getRatio());
+			assertEquals("47868.08480999999", rates.get(0).getBuy());
+			assertEquals("45984.868176", rates.get(0).getSell());
 
-            assertEquals("ETH", rates.get(1).getCode());
-            assertEquals(1, rates.get(1).getRatio());
-            assertEquals("411.74455", rates.get(1).getBuy());
-            assertEquals("389.642175", rates.get(1).getSell());
+			assertEquals("ETH", rates.get(1).getCode());
+			assertEquals(1, rates.get(1).getRatio());
+			assertEquals("3028.2716772", rates.get(1).getBuy());
+			assertEquals("2908.809696", rates.get(1).getSell());
 
-            assertEquals("XRP", rates.get(3).getCode());
-            assertEquals(1, rates.get(3).getRatio());
-            assertEquals("0.51689747", rates.get(3).getBuy()); // 0.516897472
-            assertEquals("0.47432736", rates.get(3).getSell());
+			assertEquals("XRP", rates.get(2).getCode());
+			assertEquals(1, rates.get(2).getRatio());
+			assertEquals("0.92463", rates.get(2).getBuy()); // 0.516897472
+			assertEquals("0.8836463999999999", rates.get(2).getSell());
 
-            assertEquals("DASH", rates.get(6).getCode());
-            assertEquals(1, rates.get(6).getRatio());
-            assertEquals("238.840190", rates.get(6).getBuy()); // 238.8401904
-            assertEquals("220.929696", rates.get(6).getSell());
+			assertEquals("DASH", rates.get(9).getCode());
+			assertEquals(1, rates.get(9).getRatio());
+			assertEquals("47.098604", rates.get(9).getBuy()); // 238.8401904
+			assertEquals("44.083299", rates.get(9).getSell());
 
-            assertEquals("XLM", rates.get(9).getCode());
-            assertEquals(1, rates.get(9).getRatio());
-            assertEquals("0.13281602", rates.get(9).getBuy()); // 0.1328160288
-            assertEquals("0.12159388", rates.get(9).getSell()); // 0.121593888
+			assertEquals("XLM", rates.get(5).getCode());
+			assertEquals(1, rates.get(5).getRatio());
+			assertEquals("0.21177211999999998", rates.get(5).getBuy()); // 0.1328160288
+			assertEquals("0.19804004999999997", rates.get(5).getSell()); // 0.121593888
 
-            assertEquals("Expected vs. Parsed", AltcoinsSource.CURRENCIES.size(), rates.size());
+			assertEquals("Expected vs. Parsed", AltcoinsSource.CURRENCIES.size(), rates.size());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
 
 }
