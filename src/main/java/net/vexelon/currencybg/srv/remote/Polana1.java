b1a963f958663a81c836df57fbc903e5c9b75daa
Petar Petrov <ppetrov@vexelon.net>
2023-09-02 23:21:06 +0200

    Fix Polana1 currencies parsing and unit tests

--- src/main/java/net/vexelon/currencybg/srv/remote/Polana1.java
+++ src/main/java/net/vexelon/currencybg/srv/remote/Polana1.java
@@ -1,5 +1,17 @@
 package net.vexelon.currencybg.srv.remote;
 
+import com.google.common.base.Charsets;
+import net.vexelon.currencybg.srv.Defs;
+import net.vexelon.currencybg.srv.db.models.CurrencyData;
+import net.vexelon.currencybg.srv.db.models.Sources;
+import net.vexelon.currencybg.srv.reports.Reporter;
+import net.vexelon.currencybg.srv.utils.DateTimeUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.http.HttpResponse;
+import org.jsoup.Jsoup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -7,132 +19,117 @@ import java.text.ParseException;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
-
-import net.vexelon.currencybg.srv.reports.ConsoleReporter;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.http.HttpResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
-
-import net.vexelon.currencybg.srv.Defs;
-import net.vexelon.currencybg.srv.db.models.CurrencyData;
-import net.vexelon.currencybg.srv.db.models.Sources;
-import net.vexelon.currencybg.srv.reports.Reporter;
-import net.vexelon.currencybg.srv.utils.DateTimeUtils;
+import java.util.Optional;
 
 public class Polana1 extends AbstractSource {
 
-    private static final Logger log = LoggerFactory.getLogger(Polana1.class);
-    private static final String TAG_NAME = Polana1.class.getSimpleName();
+	private static final Logger log      = LoggerFactory.getLogger(Polana1.class);
+	private static final String TAG_NAME = Polana1.class.getSimpleName();
 
-    private static final String URL_SOURCE = "https://polana1.com/bg/%D0%BE%D0%B1%D0%BC%D1%8F%D0%BD%D0%B0-%D0%BD%D0%B0-%D0%B2%D0%B0%D0%BB%D1%83%D1%82%D0%B0";
-    private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
+	private static final String URL_SOURCE  = "https://polana1.com/bg/%D0%BE%D0%B1%D0%BC%D1%8F%D0%BD%D0%B0-%D0%BD%D0%B0-%D0%B2%D0%B0%D0%BB%D1%83%D1%82%D0%B0";
+	private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
 
-    public Polana1(Reporter reporter) {
-        super(reporter);
-    }
+	public Polana1(Reporter reporter) {
+		super(reporter);
+	}
 
-    public List<CurrencyData> getPolana1(InputStream input) throws IOException, ParseException {
-        List<CurrencyData> result = Lists.newArrayList();
+	public List<CurrencyData> getPolana1(InputStream input) throws IOException, ParseException {
+		var result = new ArrayList<CurrencyData>();
 
-        Document doc = Jsoup.parse(input, Charsets.UTF_8.name(), URL_SOURCE);
+		var doc = Jsoup.parse(input, Charsets.UTF_8.name(), URL_SOURCE);
 
-        try {
-            Element contentBox = doc.select("div.content-center ").first();
-            Elements contentChildren = contentBox.children();
+		try {
+			var contentBox = doc.select("div.content-center ").first();
+			var contentChildren = contentBox.children();
 
-            String currentTimeSofia = LocalTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA))
-                    .format(DateTimeFormatter.ofPattern("HH:mm")).toString();
+			String currentTimeSofia = LocalTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA))
+					.format(DateTimeFormatter.ofPattern("HH:mm"));
 
-            String currentDateTime = contentChildren.select("h1").first().text().substring(26, 37).trim() + " "
-                    + currentTimeSofia;
-            Date updateDate = DateTimeUtils.parseDate(currentDateTime, DATE_FORMAT);
+			String currentDateTime =
+					contentChildren.select("h1").first().text().substring(26, 37).trim() + " " + currentTimeSofia;
+			var updateDate = DateTimeUtils.parseDate(currentDateTime, DATE_FORMAT);
 
-            Element contentBoxChildren = contentChildren.select("table > tbody").first();
-            Elements children = contentBoxChildren.children();
+			int row = 0;
 
-            int row = 0;
+			for (var child : Optional.ofNullable(contentChildren.select("table > tbody").first())
+					.orElseThrow(() -> new RuntimeException("table > tbody was not found!")).children()) {
+				row++;
 
-            for (Element child : children) {
-                row++;
+				try {
+					var currencyData = new CurrencyData();
+					currencyData.setDate(updateDate);
+					currencyData.setCode(child.child(1).text().replace("\u00a0", ""));
+					currencyData.setBuy(child.child(3).text().replace("\u00a0", ""));
+					currencyData.setSell(child.child(4).text().replace("\u00a0", ""));
+					currencyData.setRatio(Integer.parseInt(child.child(2).text().replace("\u00a0", "")));
+					currencyData.setSource(Sources.POLANA1.getID());
 
-                CurrencyData currencyData = new CurrencyData();
-                try {
-                    currencyData.setDate(updateDate);
-                    currencyData.setCode(child.child(1).text().replace("\u00a0", ""));
-                    currencyData.setBuy(child.child(3).text().replace("\u00a0", ""));
-                    currencyData.setSell(child.child(4).text().replace("\u00a0", ""));
-                    currencyData.setRatio(Integer.parseInt(child.child(2).text().replace("\u00a0", "")));
-                    currencyData.setSource(Sources.POLANA1.getID());
+					currencyData.setBuy(currencyData.getBuy().replace("-", ""));
+					currencyData.setSell(currencyData.getSell().replace("-", ""));
+					if ("0.".equals(currencyData.getBuy())) {
+						currencyData.setBuy("");
+					}
+					if ("0.".equals(currencyData.getSell())) {
+						currencyData.setSell("");
+					}
 
-                    currencyData.setBuy(currencyData.getBuy().replace("-", ""));
-                    currencyData.setSell(currencyData.getSell().replace("-", ""));
+					result.add(currencyData);
+				} catch (IndexOutOfBoundsException e) {
+					log.warn("Failed on row='{}', Exception={}", row, e.getMessage());
+					getReporter().write(TAG_NAME, "Could not process currency on row={}!", row + "");
+				}
+			}
 
-                    result.add(currencyData);
-                } catch (IndexOutOfBoundsException e) {
-                    log.warn("Failed on row='{}', Exception={}", row, e.getMessage());
-                    getReporter().write(TAG_NAME, "Could not process currency on row='{}'!", row + "");
-                }
-            }
+			return normalizeCurrencyData(result);
+		} catch (RuntimeException e) {
+			throw new IOException(e);
+		}
+	}
 
-            return normalizeCurrencyData(result);
-        } catch (RuntimeException e) {
-            throw new IOException(e);
-        }
-    }
+	@Override
+	public void getRates(final Callback callback) throws SourceException {
+		try {
+			doGet(URL_SOURCE, new HTTPCallback() {
 
-    @Override
-    public void getRates(final Callback callback) throws SourceException {
-        try {
-            doGet(URL_SOURCE, new HTTPCallback() {
+				@Override
+				public void onRequestFailed(Exception e) {
+					getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
 
-                @Override
-                public void onRequestFailed(Exception e) {
-                    getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
+					Polana1.this.close();
+					callback.onFailed(e);
+				}
 
-                    Polana1.this.close();
-                    callback.onFailed(e);
-                }
+				@Override
+				public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
+					var result = new ArrayList<CurrencyData>();
 
-                @Override
-                public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
-                    List<CurrencyData> result = Lists.newArrayList();
+					if (!isCanceled) {
+						try {
+							result.addAll(getPolana1(response.getEntity().getContent()));
+						} catch (IOException | ParseException e) {
+							log.error("Could not parse source data!", e);
+							getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
+						}
+					} else {
+						log.warn("Request was canceled! No currencies were downloaded.");
+						getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
+					}
 
-                    if (!isCanceled) {
-                        try {
-                            result = getPolana1(response.getEntity().getContent());
-                        } catch (IOException | ParseException e) {
-                            log.error("Could not parse source data!", e);
-                            getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
-                        }
-                    } else {
-                        log.warn("Request was canceled! No currencies were downloaded.");
-                        getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
-                    }
+					Polana1.this.close();
+					callback.onCompleted(result);
 
-                    Polana1.this.close();
-                    callback.onCompleted(result);
+				}
+			});
 
-                }
-            });
+		} catch (URISyntaxException e) {
+			throw new SourceException("Invalid source url - " + URL_SOURCE, e);
+		}
+	}
 
-        } catch (URISyntaxException e) {
-            throw new SourceException("Invalid source url - " + URL_SOURCE, e);
-        }
-    }
-
-    @Override
-    public String getName() {
-        return TAG_NAME;
-    }
-
+	@Override
+	public String getName() {
+		return TAG_NAME;
+	}
 }
--- src/test/java/net/vexelon/currencybg/srv/remote/Polana1SourceTests.java
+++ src/test/java/net/vexelon/currencybg/srv/remote/Polana1SourceTests.java
@@ -1,65 +1,62 @@
 package net.vexelon.currencybg.srv.remote;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.Test;
-
 import net.vexelon.currencybg.srv.Defs;
-import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.reports.NullReporter;
 import net.vexelon.currencybg.srv.tests.TestUtils;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class Polana1SourceTests {
 
-    @Test
-    public void test_polana1_1() {
-        try {
-            List<CurrencyData> rates = new Polana1(new NullReporter())
-                    .getPolana1(TestUtils.getTestResource("/polana1.html"));
+	@Test
+	public void test_polana1_1() {
+		try {
+			var rates = new Polana1(new NullReporter()).getPolana1(TestUtils.getTestResource("/polana1.html"));
 
-            int last = rates.size() - 1;
-            Date lastUpdate = TestUtils.newDate(2020, 2, 8,
-                    LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA)).getHour(),
-                    LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA)).getMinute());
+			assertEquals("Currencies parsed", 15, rates.size());
 
-            assertEquals("EUR", rates.get(0).getCode());
-            assertEquals(1, rates.get(0).getRatio());
-            assertEquals("1.953", rates.get(0).getBuy());
-            assertEquals("1.969", rates.get(0).getSell());
-            assertEquals(lastUpdate, rates.get(0).getDate());
+			int last = rates.size() - 1;
+			var lastUpdate = TestUtils.newDate(2023, 9, 2,
+					LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA)).getHour(),
+					LocalDateTime.now(ZoneId.of(Defs.DATETIME_TIMEZONE_SOFIA)).getMinute());
 
-            assertEquals("BAM", rates.get(last).getCode());
-            assertEquals(1, rates.get(last).getRatio());
-            assertEquals("0.98", rates.get(last).getBuy());
-            assertEquals("1.03", rates.get(last).getSell());
-            assertEquals(lastUpdate, rates.get(last).getDate());
+			assertEquals("EUR", rates.get(0).getCode());
+			assertEquals(1, rates.get(0).getRatio());
+			assertEquals("1.95", rates.get(0).getBuy());
+			assertEquals("1.962", rates.get(0).getSell());
+			assertEquals(lastUpdate, rates.get(0).getDate());
 
-            assertEquals("ALL", rates.get(last - 1).getCode());
-            assertEquals(100, rates.get(last - 1).getRatio());
-            assertEquals("1.56", rates.get(last - 1).getBuy());
-            assertEquals("1.68", rates.get(last - 1).getSell());
-            assertEquals(lastUpdate, rates.get(last - 1).getDate());
+			assertEquals("RSD", rates.get(last - 6).getCode());
+			assertEquals(100, rates.get(last - 6).getRatio());
+			assertEquals("1.67", rates.get(last - 6).getBuy());
+			assertEquals("1.75", rates.get(last - 6).getSell());
+			assertEquals(lastUpdate, rates.get(last - 6).getDate());
 
-            assertEquals("RUB", rates.get(last - 6).getCode());
-            assertEquals(100, rates.get(last - 6).getRatio());
-            assertEquals("2.75", rates.get(last - 6).getBuy());
-            assertEquals("2.89", rates.get(last - 6).getSell());
-            assertEquals(lastUpdate, rates.get(last - 6).getDate());
+			assertEquals("HUF", rates.get(last - 2).getCode());
+			assertEquals(100, rates.get(last - 2).getRatio());
+			assertEquals("0.505", rates.get(last - 2).getBuy());
+			assertEquals("0.535", rates.get(last - 2).getSell());
+			assertEquals(lastUpdate, rates.get(last - 2).getDate());
 
-            assertEquals("ILS", rates.get(last - 3).getCode());
-            assertEquals(1, rates.get(last - 3).getRatio());
-            assertEquals("", rates.get(last - 3).getBuy());
-            assertEquals("0.53", rates.get(last - 3).getSell());
-            assertEquals(lastUpdate, rates.get(last - 3).getDate());
+			assertEquals("RUB", rates.get(last - 1).getCode());
+			assertEquals(100, rates.get(last - 1).getRatio());
+			assertEquals("1.70", rates.get(last - 1).getBuy());
+			assertEquals("2.00", rates.get(last - 1).getSell());
+			assertEquals(lastUpdate, rates.get(last - 1).getDate());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
+			assertEquals("PLN", rates.get(last).getCode());
+			assertEquals(1, rates.get(last).getRatio());
+			assertEquals("0.425", rates.get(last).getBuy());
+			assertEquals("0.445", rates.get(last).getSell());
+			assertEquals(lastUpdate, rates.get(last).getDate());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
 }
