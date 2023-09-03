0c961a3c8c0e035ffba484b27f4228df6e791440
Petar Petrov <ppetrov@vexelon.net>
2023-09-03 11:17:58 +0200

    Fix Unicredit unit tests

--- src/main/java/net/vexelon/currencybg/srv/remote/UnicreditSource.java
+++ src/main/java/net/vexelon/currencybg/srv/remote/UnicreditSource.java
@@ -1,137 +1,128 @@
 package net.vexelon.currencybg.srv.remote;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.text.ParseException;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.http.HttpResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
-
 import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.db.models.Sources;
 import net.vexelon.currencybg.srv.reports.Reporter;
 import net.vexelon.currencybg.srv.utils.DateTimeUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.http.HttpResponse;
+import org.jsoup.Jsoup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class UnicreditSource extends AbstractSource {
 
-    private static final Logger log = LoggerFactory.getLogger(UnicreditSource.class);
-    private static final String TAG_NAME = UnicreditSource.class.getSimpleName();
+	private static final Logger log      = LoggerFactory.getLogger(UnicreditSource.class);
+	private static final String TAG_NAME = UnicreditSource.class.getSimpleName();
 
-    private static final String URL_SOURCE = "https://www.unicreditbulbank.bg/bg/valutni-kursove/";
-    private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
+	private static final String URL_SOURCE  = "https://www.unicreditbulbank.bg/bg/valutni-kursove/";
+	private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
 
-    public UnicreditSource(Reporter reporter) {
-        super(reporter);
-    }
+	public UnicreditSource(Reporter reporter) {
+		super(reporter);
+	}
 
-    public List<CurrencyData> getUnicreditRates(InputStream input) throws IOException, ParseException {
-        List<CurrencyData> result = Lists.newArrayList();
+	public List<CurrencyData> getUnicreditRates(InputStream input) throws IOException, ParseException {
+		var result = new ArrayList<CurrencyData>();
 
-        Document doc = Jsoup.parse(input, Charsets.UTF_8.name(), URL_SOURCE);
+		var doc = Jsoup.parse(input, Charsets.UTF_8.name(), URL_SOURCE);
 
-        try {
-            // Parse date and time
-            Element dateAttribute = doc
-                    .select("div.container > div.gray-background.exchnage-form-container > div.row > div.col-xs-12.col-sm-12 > form#index_table_form.form-inline.searchform > div.form-group.col-xs-12.col-sm-3.col-md-3 > div.controls > input[name=date]")
-                    .first();
-            String date = dateAttribute.attr("value");
+		try {
+			// Parse date and time
+			var dateAttribute = doc.select(
+							"div.container > div.gray-background.exchnage-form-container > div.row > div.col-xs-12.col-sm-12 > form#index_table_form.form-inline.searchform > div.form-group.col-xs-12.col-sm-3.col-md-3 > div.controls > input[name=date]")
+					.first();
+			String date = Objects.requireNonNull(dateAttribute, "input[name=date] was not found").attr("value");
 
-            Element timeArrtibute = doc
-                    .select("div.container > div.gray-background.exchnage-form-container > div.row > div.col-xs-12.col-sm-12 > form#index_table_form.form-inline.searchform > div.form-group.col-xs-12.col-sm-3.col-md-3 > div.controls.clockpicker > input[name=time]")
-                    .first();
-            String time = timeArrtibute.attr("value");
+			var timeAttribute = doc.select(
+							"div.container > div.gray-background.exchnage-form-container > div.row > div.col-xs-12.col-sm-12 > form#index_table_form.form-inline.searchform > div.form-group.col-xs-12.col-sm-3.col-md-3 > div.controls.clockpicker > input[name=time]")
+					.first();
+			String time = Objects.requireNonNull(timeAttribute, "input[name=time] was not found!").attr("value");
 
-            Date updateDate = DateTimeUtils.parseDate(date.replace("/", ".") + " " + time, DATE_FORMAT);
+			var updateDate = DateTimeUtils.parseDate(date.replace("/", ".") + " " + time, DATE_FORMAT);
 
-            // Parse data content
-            Element content = doc
-                    .select("div.container > div.index-currency-table > div.row > div.col-xs-12 > table.table--exchange.table--exchange--responsive > tbody")
-                    .first();
+			// Parse currencies content
+			int row = 0;
 
-            Elements children1 = content.children();
+			for (var child : Objects.requireNonNull(doc.select(
+							"div.container > div.index-currency-table > div.row > div.col-xs-12 > table.table--exchange.table--exchange--responsive > tbody")
+					.first(), "content was not found!").children()) {
+				try {
+					if (row > 0) {
+						var currencyData = new CurrencyData();
+						currencyData.setDate(updateDate);
+						currencyData.setCode(child.child(0).text());
+						currencyData.setBuy(child.child(2).text().replace(",", "."));
+						currencyData.setSell(child.child(3).text().replace(",", "."));
+						currencyData.setRatio(Integer.parseInt(child.child(1).text()));
+						currencyData.setSource(Sources.UNICREDIT.getID());
+						result.add(currencyData);
+					}
 
-            int row = 0;
-            for (Element child : children1) {
-                CurrencyData currencyData = new CurrencyData();
-                try {
-                    if (row > 0) {
-                        currencyData.setDate(updateDate);
-                        currencyData.setCode(child.child(0).text());
-                        currencyData.setBuy(child.child(2).text().replace(",", "."));
-                        currencyData.setSell(child.child(3).text().replace(",", "."));
-                        currencyData.setRatio(Integer.parseInt(child.child(1).text()));
-                        currencyData.setSource(Sources.UNICREDIT.getID());
-                        result.add(currencyData);
-                    }
+				} catch (IndexOutOfBoundsException e) {
+					log.warn("Failed on row={}, Exception={}", row, e.getMessage());
+					getReporter().write(TAG_NAME, "Could not process currency on row={}!", row + "");
+				}
 
-                } catch (IndexOutOfBoundsException e) {
-                    log.warn("Failed on row='{}', Exception={}", row, e.getMessage());
-                    getReporter().write(TAG_NAME, "Could not process currency on row='{}'!", row + "");
-                }
+				row++;
+			}
 
-                row++;
+			return normalizeCurrencyData(result);
+		} catch (RuntimeException e) {
+			throw new IOException(e);
+		}
+	}
 
-            }
+	@Override
+	public void getRates(Callback callback) throws SourceException {
+		try {
+			doGet(URL_SOURCE, new HTTPCallback() {
 
-            return normalizeCurrencyData(result);
-        } catch (RuntimeException e) {
-            throw new IOException(e);
-        }
-    }
+				@Override
+				public void onRequestFailed(Exception e) {
+					getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
 
-    @Override
-    public void getRates(Callback callback) throws SourceException {
-        try {
-            doGet(URL_SOURCE, new HTTPCallback() {
+					UnicreditSource.this.close();
+					callback.onFailed(e);
+				}
 
-                @Override
-                public void onRequestFailed(Exception e) {
-                    getReporter().write(TAG_NAME, "Connection failure= {}", ExceptionUtils.getStackTrace(e));
+				@Override
+				public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
+					var result = new ArrayList<CurrencyData>();
 
-                    UnicreditSource.this.close();
-                    callback.onFailed(e);
-                }
+					if (!isCanceled) {
+						try {
+							result.addAll(getUnicreditRates(response.getEntity().getContent()));
+						} catch (IOException | ParseException e) {
+							log.error("Could not parse source data!", e);
+							getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
+						}
+					} else {
+						log.warn("Request was canceled! No currencies were downloaded.");
+						getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
+					}
+					UnicreditSource.this.close();
+					callback.onCompleted(result);
+				}
+			});
+		} catch (URISyntaxException e) {
+			throw new SourceException("Invalid source url - " + URL_SOURCE, e);
+		}
 
-                @Override
-                public void onRequestCompleted(HttpResponse response, boolean isCanceled) {
-                    List<CurrencyData> result = Lists.newArrayList();
+	}
 
-                    if (!isCanceled) {
-                        try {
-                            result = getUnicreditRates(response.getEntity().getContent());
-                        } catch (IOException | ParseException e) {
-                            log.error("Could not parse source data!", e);
-                            getReporter().write(TAG_NAME, "Parse failed= {}", ExceptionUtils.getStackTrace(e));
-                        }
-                    } else {
-                        log.warn("Request was canceled! No currencies were downloaded.");
-                        getReporter().write(TAG_NAME, "Request was canceled! No currencies were downloaded.");
-                    }
-                    UnicreditSource.this.close();
-                    callback.onCompleted(result);
-                }
-            });
-        } catch (URISyntaxException e) {
-            throw new SourceException("Invalid source url - " + URL_SOURCE, e);
-        }
-
-    }
-
-    @Override
-    public String getName() {
-        return TAG_NAME;
-    }
+	@Override
+	public String getName() {
+		return TAG_NAME;
+	}
 
 }
--- src/test/java/net/vexelon/currencybg/srv/remote/UnicreditSourceTests.java
+++ src/test/java/net/vexelon/currencybg/srv/remote/UnicreditSourceTests.java
@@ -1,49 +1,49 @@
 package net.vexelon.currencybg.srv.remote;
 
-import java.util.Date;
-import java.util.List;
-
-import org.junit.Test;
-
 import net.vexelon.currencybg.srv.db.models.CurrencyData;
 import net.vexelon.currencybg.srv.reports.NullReporter;
 import net.vexelon.currencybg.srv.tests.TestUtils;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class UnicreditSourceTests {
 
-    @Test
-    public void test_inicredit_1() {
-        try {
-            List<CurrencyData> rates = new UnicreditSource(new NullReporter())
-                    .getUnicreditRates(TestUtils.getTestResource("/unicredit.html"));
+	@Test
+	public void test_unicredit_1() {
+		try {
+			List<CurrencyData> rates = new UnicreditSource(new NullReporter()).getUnicreditRates(
+					TestUtils.getTestResource("/unicredit.html"));
 
-            int last = rates.size() - 1;
-            Date lastUpdate = TestUtils.newDate(2020, 02, 8, 21, 38); //
+			assertEquals("Currencies parsed", 17, rates.size());
 
-            assertEquals("EUR", rates.get(0).getCode());
-            assertEquals(1, rates.get(0).getRatio());
-            assertEquals("1.945000", rates.get(0).getBuy());
-            assertEquals("1.960000", rates.get(0).getSell());
-            assertEquals(lastUpdate, rates.get(0).getDate());
+			int last = rates.size() - 1;
+			Date lastUpdate = TestUtils.newDate(2023, 9, 3, 12, 12);
 
-            assertEquals("TRY", rates.get(last).getCode());
-            assertEquals(10, rates.get(last).getRatio());
-            assertEquals("2.804000", rates.get(last).getBuy());
-            assertEquals("3.101000", rates.get(last).getSell());
-            assertEquals(lastUpdate, rates.get(last).getDate());
+			assertEquals("EUR", rates.get(0).getCode());
+			assertEquals(1, rates.get(0).getRatio());
+			assertEquals("1.945000", rates.get(0).getBuy());
+			assertEquals("1.960000", rates.get(0).getSell());
+			assertEquals(lastUpdate, rates.get(0).getDate());
 
-            assertEquals("RSD", rates.get(last - 3).getCode());
-            assertEquals(100, rates.get(last - 3).getRatio());
-            assertEquals("1.580000", rates.get(last - 3).getBuy());
-            assertEquals("1.746000", rates.get(last - 3).getSell());
-            assertEquals(lastUpdate, rates.get(last - 3).getDate());
+			assertEquals("HUF", rates.get(last - 7).getCode());
+			assertEquals(1000, rates.get(last - 7).getRatio());
+			assertEquals("4.848000", rates.get(last - 7).getBuy());
+			assertEquals("5.366000", rates.get(last - 7).getSell());
+			assertEquals(lastUpdate, rates.get(last - 7).getDate());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
+			assertEquals("TRY", rates.get(last).getCode());
+			assertEquals(100, rates.get(last).getRatio());
+			assertEquals("6.410000", rates.get(last).getBuy());
+			assertEquals("7.130000", rates.get(last).getSell());
+			assertEquals(lastUpdate, rates.get(last).getDate());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
 }
